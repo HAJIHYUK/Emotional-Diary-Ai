b58f3de8502e873c8c7f78e2240cb4ca09f67367
HAJIHYUK <168079884+HAJIHYUK@users.noreply.github.com>
2025-12-31 19:49:34 +0900

    HomeController 주석처리, jsp로 개발할때 사용하던 컨트롤러, aws 서버 올리는 도중 WebController와 충돌하여 주석처리

--- src/main/java/com/jh/emotion/controller/HomeController.java
+++ src/main/java/com/jh/emotion/controller/HomeController.java
@@ -1,43 +1,43 @@
-package com.jh.emotion.controller;
+// package com.jh.emotion.controller;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+// import org.springframework.stereotype.Controller;
+// import org.springframework.web.bind.annotation.GetMapping;
 
-@Controller
-public class HomeController {
+// @Controller // jsp 로 개발할때 사용하던 컨트롤러  
+// public class HomeController {
 
-    @GetMapping("/")
-    public String home() {
-        return "home";
-    }
+//     @GetMapping("/")
+//     public String home() {
+//         return "home";
+//     }
 
-    @GetMapping("/diary")
-    public String diary() {
-        return "diary_write";
-    }
+//     @GetMapping("/diary")
+//     public String diary() {
+//         return "diary_write";
+//     }
 
-    @GetMapping("/user-preference")
-    public String userPreference() {
-        return "user_preference_input";
-    }
+//     @GetMapping("/user-preference")
+//     public String userPreference() {
+//         return "user_preference_input";
+//     }
 
-    @GetMapping("/naver-search")
-    public String naverSearch() {
-        return "naver_search";
-    }
+//     @GetMapping("/naver-search")
+//     public String naverSearch() {
+//         return "naver_search";
+//     }
 
-    @GetMapping("/kakaomap")
-    public String kakaomap() {
-        return "kakaomap";
-    }
+//     @GetMapping("/kakaomap")
+//     public String kakaomap() {
+//         return "kakaomap";
+//     }
 
-    @GetMapping("/emotion-stats")
-    public String emotionStats() {
-        return "emotion_stats_test";
-    }
+//     @GetMapping("/emotion-stats")
+//     public String emotionStats() {
+//         return "emotion_stats_test";
+//     }
 
-    @GetMapping("/user-click-event")
-    public String userClickEvent() {
-        return "user_click_event";
-    }
-} 
+//     @GetMapping("/user-click-event")
+//     public String userClickEvent() {
+//         return "user_click_event";
+//     }
+// } 
